72f492d829848015339dab05b81c0469e14343d5
juan@owsas.com <juan@owsas.com>
2018-05-17 15:16:43 -0500

    tested

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export class MyModule {
-  
+export class ParseService {
+  static find(
+    query: Parse.Query, 
+    options?: Parse.Query.FindOptions,
+  ): Parse.Promise<Parse.Object[]> {
+    return query.find(options);
+  }
+
+  static first(
+    query: Parse.Query, 
+    options?: Parse.Query.FirstOptions,
+  ): Parse.Promise<Parse.Object> {
+    return query.first(options);
+  }
+
+  static save(obj:Parse.Object, options: Parse.Object.SaveOptions):
+  Parse.Promise<Parse.Object> {
+    return obj.save(null, options);
+  }
+
+  static destroy(obj:Parse.Object, options: Parse.Object.DestroyOptions):
+  Parse.Promise<Parse.Object> {
+    return obj.destroy(options);
+  }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,67 @@
-import { MyModule } from '../src/index';
+import * as Parse from 'parse/node';
+import { ParseService } from '../src/index';
 
-test('test', () => {
-  expect(true).toBe(true);
+describe('#find', () => {
+  test('should call the find method of the query', async () => {
+    const obj = new Parse.Query('Test');
+    
+    const mock = jest.spyOn(obj, 'find');
+    mock.mockImplementation(async (params, options) => obj);
+
+    const options = { useMasterKey: true };
+
+    const result = await ParseService.find(obj, options);
+
+    expect(mock).toHaveBeenCalledWith(options);
+    expect(result).toEqual(obj);
+  });
 });
+
+describe('#first', () => {
+  test('should call the first method of the query', async () => {
+    const obj = new Parse.Query('Test');
+    
+    const mock = jest.spyOn(obj, 'first');
+    mock.mockImplementation(async (params, options) => obj);
+
+    const options = { useMasterKey: true };
+
+    const result = await ParseService.first(obj, options);
+
+    expect(mock).toHaveBeenCalledWith(options);
+    expect(result).toEqual(obj);
+  });
+});
+
+describe('#save', () => {
+  test('should call the save method of the object', async () => {
+    const obj = new Parse.Object('Test');
+    
+    const mock = jest.spyOn(obj, 'save');
+    mock.mockImplementation(async (params, options) => obj);
+
+    const options = { useMasterKey: true };
+
+    const result = await ParseService.save(obj, options);
+
+    expect(mock).toHaveBeenCalledWith(null, options);
+    expect(result).toEqual(obj);
+  });
+}); 
+
+describe('#destroy', () => {
+  test('should call the destroy method of the object', async () => {
+    const obj = new Parse.Object('Test');
+    
+    const mock = jest.spyOn(obj, 'destroy');
+    mock.mockImplementation(async options => obj);
+
+    const options = { useMasterKey: true };
+
+    const result = await ParseService.destroy(obj, options);
+
+    expect(mock).toHaveBeenCalledWith(options);
+    expect(result).toEqual(obj);
+  });
+});
+
